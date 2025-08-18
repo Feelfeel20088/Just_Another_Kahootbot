e9391ece377db7366754c1021d6da845eb9c6627
Felix <feelfeel200088@gmail.com>
2025-08-17 23:57:37 -0500

    stuffs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(), # Automatically find all packages in the project
     entry_points={
         'console_scripts': [
-            'just_another_kahootbot = justAnotherKahootBot.__main__:main',
+            'just_another_kahootbot = .justAnotherKahootBot.__main__:main',
         ],
     },
 )
