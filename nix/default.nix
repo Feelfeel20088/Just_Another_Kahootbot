25d68586883115c10cdfbb838c9f21565402653c
felix <feelfeel200088@gmail.com>
2025-09-15 17:43:14 -0500

    fix nix stuff. stage for modules.

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,7 @@
 {
   pkgs,
   python313Packages,
-  callPackage,
 }:
-let 
-nodeDeps = callPackage ./node-packages.nix {};
-in 
 
 with pkgs; 
 
@@ -13,7 +9,13 @@ python313Packages.buildPythonApplication {
   name = "just_another_kahootbot";
   version = "v0.2.0-alpha";
 
-  src = ../.;
+    src = pkgs.fetchFromGitHub {
+      owner = "Feelfeel20088";
+      repo = "Just_Another_Kahootbot";
+      rev = "v0.3.0-alpha"; 
+      sha256 = "sha256-7Obo7nW3ZeFHPUt/cXlgbrGP09GlQGRFwcZAiMaSomo="; # required
+    };
+
 
   propagatedBuildInputs = (with python313Packages; [
     httpx
@@ -22,13 +24,14 @@ python313Packages.buildPythonApplication {
     pydantic
     orjson
     
-  ]) ++ [ nodejs_24 ] ++ builtins.attrValues nodeDeps;
+  ]) ++ [ nodejs_24 ];
 
-    # for docker
+  # for docker
   postInstall = ''
     mkdir -p $out/app
     cp -r . $out/app/
   '';
+  
   meta = {
     description = "Just_Another_Kahoot_Bot is a highly scalable, single-threaded bot for Kahoot, built for deployment on Kubernetes. Unlike traditional bots that rely on Selenium, this bot uses raw WebSockets, providing better performance, stability, and reliability. The bot can flood Kahoot games, answer questions correctly, and remain stealthy.";
     homepage = "https://github.com/Feelfeel20088/Just_Another_Kahootbot";
