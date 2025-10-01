a7ae76ee772d38957629f2b8980a7f58f9cfa4ba
felix <felix@felixhub.dev>
2025-10-01 18:36:06 -0500

    fix cuz im dum

--- justAnotherKahootBot/api/status.py
+++ justAnotherKahootBot/api/status.py
@@ -2,6 +2,7 @@ from justAnotherKahootBot.kahootBot.swarm import Swarm
 from quart import request, jsonify
 from . import app
 from .context import Context 
+from typing import cast
 
 
 context = Context()
@@ -10,9 +11,10 @@ context = Context()
 async def status():
     """Returns the status of all active swarms. also will clean up dead swarms."""
     swarm_info = []
-    for swarm, index in enumerate(context.get_swarm_list()):
-            
+    for index, swarm in enumerate(context.getSwarmList()):
+
         swarm_info.append({
+            "swarm_uuid": swarm.getSwarmId(),
             "swarm": index,
             "time_remaining": swarm.getTimeRemaining(),
             "active_bots": len(swarm.tasks)
