72437c688f7248fd77e374e6d702da7e8c0ff974
felix <felix@felixhub.dev>
2025-10-26 22:58:25 -0500

    remove(flake.nix) remove unused pkgs in flake.nix - removed yarn - removed node

--- flake.nix
+++ flake.nix
@@ -56,8 +56,6 @@
             pkgs.python313Packages.orjson
             pkgs.python313Packages.pytest
             pkgs.python313Packages.configargparse
-            pkgs.nodejs
-            pkgs.yarn
 
             # linter
             # pkgs.python313Packages.ruff
