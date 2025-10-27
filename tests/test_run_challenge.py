9ef376461587acf4e695dc912dce24573094ced4
felix <felix@felixhub.dev>
2025-10-26 23:04:31 -0500

    feat(testing) add proper working test + ci - add ci to call pytest - add single test as proof of concept

--- tests/test_run_challenge.py
+++ tests/test_run_challenge.py
@@ -2,6 +2,7 @@ from justAnotherKahootBot.challenge.runchallenge import Challenge
 
 
 def test_run_challenge():
-    js_challenge_str = "decode.call(a, 'rhb1pEIIrz4Bvx4RNlVPdLM8I7VQjaPs7tGlByc2J1erBztNslMoMBmX4LNWVBTHu1OPYHShEMRYWoBu324RhFI6HrgElpGpqm60'); function decode(message) { var offset = (59 + 5) * (95 + 91) * 5; if (a.angular.isArray(offset)) console.log(\"Offset derived as: {\", offset, \"}\"); return _.replace(message, /./g, function (char, position) { return String.fromCharCode((((char.charCodeAt(0) * position) + offset) % 77) + 48); }); }"
-    client_id = "A1VUNl9+NWJYYAF2RgY1VTNPUlcAVS0iVBBAAERLS3MBRRZ4NVZVHgxSYihDA2wDZi5AVUxpBl02XwU6AklyZwxeVVJnfgpTBQ8NLmtVY1oIUgM6B05GL1R+PHBCUGtl"
-    assert Challenge.run_challenge(js_challenge_str, client_id) == "b33fc2f862e61dfbf77bb4fc5f852279b2b4ff7d498f47380b3c4061e01de274359025e51d4f6498a58af73fa6f57972"
+    js_challenge_str = js_challenge_str = "decode.call(this, 'CHH7d8RTfdxAxhQMN2Qxlg81ELRIj2YTer6aeQLSslnI6djb42bmEj9K1I8NMLEcWELPCu9NF7EB3Q3vWtXreXM7FeWfOaT6dVUt'); function decode(message) {var offset = 87*99+((86*(10*12))*99+59); if(this.angular.isDate(offset)) console.log('Offset derived as:', offset); return _.replace(message,/./g,function(char,position){return String.fromCharCode((((char.charCodeAt(0)*position)+offset)%77)+48);});}"
+
+    client_id = "DgpRK31HPkdVEVVmRFxFCX4jA1tzdgoTVUNBWw9UFQYJBgsnNlxVchpTDkwNAmhSaVRHKggIX18TI39OCHlbB2FjewwuVwQKUHIxZW9bEyVleEMBfyxdeggeTQplfVpf"
+    assert Challenge.run_challenge(js_challenge_str, client_id) == "10da37c78e9316261a95005c3056fff7428afe30b5169c0c6e7de4b4ca247618611bbd5328f06daa89572fb07f9c8630"
