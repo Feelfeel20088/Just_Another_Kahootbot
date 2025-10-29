e5c780d969b4a337dc93ac143044ab157ab05b61
felix <felix@felixhub.dev>
2025-10-28 20:35:47 -0500

    fix(logger) and manual override for log_dir for testing

--- justAnotherKahootBot/config/logger.py
+++ justAnotherKahootBot/config/logger.py
@@ -3,39 +3,36 @@ from pathlib import Path
 import os
 from justAnotherKahootBot.config.state import args 
 
-log_dir = args.log_dir
-verbose_level = args.verbose
+
 
 logger = logging.getLogger(__name__)
 
 
 
-def setup_logger():
+def setup_logger(log_dir: str = None):
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     
-    # StreamHandler for logging to the console
+    # StreamHandler
     ch = logging.StreamHandler()
-    if verbose_level == 0:
+    if args.verbose == 0:
         ch.setLevel(logging.CRITICAL)
-    elif verbose_level == 1:
+    elif args.verbose == 1:
         ch.setLevel(logging.ERROR)
-    elif verbose_level == 2:
+    elif args.verbose == 2:
         ch.setLevel(logging.WARNING)
-    elif verbose_level == 3:
+    elif args.verbose == 3:
         ch.setLevel(logging.DEBUG)
-    
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    os.makedirs(log_dir, exist_ok=True)
-
-    log_file = Path(os.path.join(log_dir, "logs.log"))
+    # Use overridden log_dir if provided
+    effective_log_dir = log_dir or args.log_dir
+    os.makedirs(effective_log_dir, exist_ok=True)
+    log_file = Path(os.path.join(effective_log_dir, "logs.log"))
     log_file.touch(exist_ok=True)
     
     fh = logging.FileHandler(log_file, mode='a')
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
-
     logger.addHandler(fh)
-    
