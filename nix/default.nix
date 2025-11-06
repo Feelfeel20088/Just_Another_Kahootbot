260b309d8f11cda4b868bad92f7ad670c03ced15
felix <felix@felixhub.dev>
2025-11-05 19:55:55 -0600

    fix(nix) change license

--- nix/default.nix
+++ nix/default.nix
@@ -49,7 +49,7 @@ python313Packages.buildPythonApplication {
   meta = {
     description = "Just_Another_Kahoot_Bot is a highly scalable, single-threaded bot for Kahoot, built for deployment on Kubernetes. Unlike traditional bots that rely on Selenium, this bot uses raw WebSockets, providing better performance, stability, and reliability. The bot can flood Kahoot games, answer questions correctly, and remain stealthy.";
     homepage = "https://github.com/Feelfeel20088/Just_Another_Kahootbot";
-    license = lib.licenses.gpl3Only;
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ Feelfeel20088 ];
   };
 }
